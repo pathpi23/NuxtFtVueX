12a467bc03736aa84ead260145174a71217620ae
tnp.am <thanaporn.piti@mail.kmutt.ac.th>
2020-06-23 15:21:09 +0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>  Counter is : {{$store.state.counter }}</div>
+    <div> <button @click="increment"> Add data</button></div>
+  </div>
+</template>
+<script>
+  import { mapState } from 'vuex'
+export default {
+  // fetch(context) is called by the server-side
+  // and before instantiating the component
+  fetch ({ store }) {
+    store.commit('increment')
+  },
+  computed: mapState([
+    'counter'
+  ]),
+  methods: {
+    increment () {
+      this.$store.commit('increment')
+    }
+  }
+}
+</script>
